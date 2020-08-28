be88bc62f4f997c58c4e25b427b8fcc4aa555401
suhasspatil <32064436+suhasspatil@users.noreply.github.com>
2020-08-28 11:45:36 +0530

    Adding Constant class

---
+++ MAPSOrderProcessingSolution/OrderProcessingDataModel/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessingDataModel
+{
+    static class Constants
+    {
+        public static List<string> OrderStatuses = new List<string>();
+    }
+}
