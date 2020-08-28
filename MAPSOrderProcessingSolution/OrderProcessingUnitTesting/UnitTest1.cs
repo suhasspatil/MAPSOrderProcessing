d4bc4b70644b4f94167ce389479b9b8e1c7a9e8c
suhasspatil <32064436+suhasspatil@users.noreply.github.com>
2020-08-28 13:08:26 +0530

    Test 3 - testRuleForMembership

--- MAPSOrderProcessingSolution/OrderProcessingUnitTesting/UnitTest1.cs
+++ MAPSOrderProcessingSolution/OrderProcessingUnitTesting/UnitTest1.cs
@@ -50,5 +50,26 @@ namespace OrderProcessingUnitTesting
             Assert.AreEqual(checkList[0], Constants.OrderStatuses[0]);
             Assert.AreEqual(checkList[1], Constants.OrderStatuses[1]);
         }
+
+        [TestMethod]
+        public void testRuleForMembership()
+        {
+            Order ord = new Order
+            {
+                OrderID = 001,
+                OrderDate = DateTime.Now,
+                PType = PaymentType.Membership
+            };
+            OrderProcessing orderProcessingService = new OrderProcessing();
+            orderProcessingService.ProcessOrder(ord);
+            List<string> checkList = new List<string>()
+            {
+                "Activate Membership",
+                "Send Activation Email to User"
+            };
+
+            Assert.AreEqual(checkList[0], Constants.OrderStatuses[0]);
+            Assert.AreEqual(checkList[1], Constants.OrderStatuses[1]);
+        }
     }
 }
