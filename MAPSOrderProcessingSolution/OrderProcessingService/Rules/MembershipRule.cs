584e488d6ed7262f74de8decd768421fa936ba30
suhasspatil <32064436+suhasspatil@users.noreply.github.com>
2020-08-28 11:57:25 +0530

    Membership & Upgrade Membership Payment type Rules

--- MAPSOrderProcessingSolution/OrderProcessingService/Rules/MembershipRule.cs
+++ MAPSOrderProcessingSolution/OrderProcessingService/Rules/MembershipRule.cs
@@ -6,11 +6,26 @@ using OrderProcessingService.Interface;
 
 namespace OrderProcessingService.Rules
 {
+    //Membership Rule - Handle Membership & Upgrade Membership Payment type Rules
     public class MembershipRule : IOrderProcessingRule
     {
         public void ExecuteRule(Order order)
         {
-            throw new NotImplementedException();
+            //Payment is for Membership
+            if (order.PType == PaymentType.Membership)
+            {
+                Constants.OrderStatuses.Add("Activate Membership");
+            }
+            //Payment is for UpgradeMembership
+            if (order.PType == PaymentType.UpgradeMembership)
+            {
+                Constants.OrderStatuses.Add("Membership Upgrade");
+            }
+            //Payment is for Membership or UpgradeMembership
+            if (order.PType == PaymentType.Membership || order.PType == PaymentType.UpgradeMembership)
+            {
+                Constants.OrderStatuses.Add("Send Activation Email to User");
+            }
         }
     }
 }
