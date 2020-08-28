86bd64feffc9be9b2cdb0306d2449c598c435dfb
suhasspatil <32064436+suhasspatil@users.noreply.github.com>
2020-08-28 11:38:15 +0530

    Generic Order Processing for all rules changes

--- MAPSOrderProcessingSolution/OrderProcessingService/Service/OrderProcessingService.cs
+++ MAPSOrderProcessingSolution/OrderProcessingService/Service/OrderProcessingService.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Text;
 using OrderProcessingDataModel;
 using OrderProcessingService.Interface;
+using OrderProcessingService.Rules;
 
 namespace OrderProcessingService
 {
+    //Problem Statement 2 : Business Rule Engine for Order Processing 
     public class OrderProcessingService : IOrderProcessing
     {
+        //Order Prcoessing List - Rules needs to be executed on Order
+        List<IOrderProcessingRule> Rules = new List<IOrderProcessingRule>();
+
+        //Constructor
+        public OrderProcessingService()
+        {
+            //Adding Physical Product Rule - Handle Physical Product & Book Payment type
+            Rules.Add(new PhysicalProductRule());
+            //Adding Membership Rule Rule - Handle Membership & Upgrade Membership Payment type
+            Rules.Add(new MembershipRule());
+            //Adding Video Learning Rule - Handle Video 'Learning to Ski' Payment type
+            Rules.Add(new VideoLearningRule());
+
+            //New Rules can be added here for future requirment . 
+           
+        }
+
+        //Process Order handle all kind of Orders .
         public List<string> ProcessOrder(Order order)
         {
-            throw new NotImplementedException();
+            foreach(IOrderProcessingRule rule in Rules)
+            {
+                rule.ExecuteRule(order);
+            }
+
+            return new List<string>();
         }
     }
 }
